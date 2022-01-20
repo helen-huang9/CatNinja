8f1d9cd7c9c65453783083340bb9ea3f108149c5
Helen Huang <helen_huang@brown.edu>
2022-01-19 21:12:58 -0500

    added bear image

--- CatNinja/GameSceneView.swift
+++ CatNinja/GameSceneView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import SpriteKit
 
 struct GameSceneView: View {
-    let scene = SKScene(fileNamed: "GameScene")!
+    var scene = SKScene(fileNamed: "GameScene")!
     
     var body: some View {
         SpriteView(scene: scene)
