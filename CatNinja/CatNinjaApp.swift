8339ca2841bfd466e4e3f3bb3c2c93eaddb681b8
Helen Huang <helen_huang@brown.edu>
2022-01-19 20:19:09 -0500

    Initial Commit

---
+++ CatNinja/CatNinjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatNinjaApp.swift
+//  CatNinja
+//
+//  Created by Helen Huang on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct CatNinjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
