eb17eb243c0f848c0db208503292629811936b2c
Helen Huang <helen_huang@brown.edu>
2022-01-29 15:24:36 -0500

    joined two yarn fragments

--- CatNinja/GameScene.swift
+++ CatNinja/GameScene.swift
@@ -10,7 +10,26 @@ import SpriteKit
 class GameScene: SKScene {
     
     override func sceneDidLoad() {
-        self.addChild(SKSpriteNode(imageNamed: "yarn"))
+        let firstHalf = SKSpriteNode(imageNamed: "yarn-half-2")
+        firstHalf.name = "first half"
+        firstHalf.setScale(0.2)
+        firstHalf.physicsBody = SKPhysicsBody(texture: SKTexture(imageNamed: "yarn-half-2"), size: firstHalf.size)
+        firstHalf.physicsBody?.velocity = CGVector(dx: 50, dy: -50)
+        firstHalf.anchorPoint = CGPoint(x: 0.5, y: 0)
+        
+        let secondHalf = SKSpriteNode(imageNamed: "yarn-half-1")
+        secondHalf.name = "second half"
+        secondHalf.setScale(0.2)
+        secondHalf.physicsBody = SKPhysicsBody(texture: SKTexture(imageNamed: "yarn-half-1"), size: secondHalf.size)
+        secondHalf.anchorPoint = CGPoint(x: 0.5, y: 1)
+        
+        self.addChild(firstHalf)
+        self.addChild(secondHalf)
+        
+        let mid = CGPoint(x: 0, y: 0)
+        let firstHalfJoint = SKPhysicsJointFixed.joint(withBodyA: firstHalf.physicsBody!, bodyB: secondHalf.physicsBody!, anchor: mid)
+        
+        self.physicsWorld.add(firstHalfJoint)
     }
     
     override var isUserInteractionEnabled: Bool {
@@ -34,19 +53,19 @@ class GameScene: SKScene {
     }
     
     override func update(_ currentTime: TimeInterval) {
-        self.children.forEach { node in
-            guard let view = self.view else {
-                return
-            }
-            let minX = view.bounds.minX
-            let minY = view.bounds.minY
-            let maxX = view.bounds.maxX
-            let maxY = view.bounds.maxY
-            
-            let x = CGFloat.random(in: minX ... maxX) - view.bounds.midX
-            let y = CGFloat.random(in: minY ... maxY) - view.bounds.midY
-            
-            node.position = CGPoint(x: x, y: y)
-        }
+//        self.children.forEach { node in
+//            guard let view = self.view else {
+//                return
+//            }
+//            let minX = view.bounds.minX
+//            let minY = view.bounds.minY
+//            let maxX = view.bounds.maxX
+//            let maxY = view.bounds.maxY
+//
+//            let x = CGFloat.random(in: minX ... maxX) - view.bounds.midX
+//            let y = CGFloat.random(in: minY ... maxY) - view.bounds.midY
+//
+//            node.position = CGPoint(x: x, y: y)
+//        }
     }
 }
